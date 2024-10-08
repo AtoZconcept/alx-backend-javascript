612449da5d1dbcda797ba4f028f797b764f195e2
AtoZconcept <opeyemiolaafeez.com>
2024-10-08 17:34:02 +0100

    add

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 /* variables aren’t overwritten inside the conditional block */
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
+    let task = false;
+    let task2 = true;
 
     if (trueOrFalse) {
-      task = true;
-      task2 = false;
+      let task = true;
+      let task2 = false;
     }
 
     return [task, task2];
