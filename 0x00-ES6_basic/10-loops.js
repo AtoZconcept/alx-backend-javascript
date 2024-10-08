6c1590967a86562a6ca3bb14cfec82da1043e532
AtoZconcept <opeyemiolaafeez.com>
2024-10-08 18:49:25 +0100

    add

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const newArr = [];
+
+    for (const item of array) {
+      newArr.push(appendString + item);
+    }
+
+    return newArr;
+  }
